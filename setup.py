e0b745c4bfb285e53a64c447cfb3fd5d8568e587
jdl <joedlyman@gmail.com>
2020-11-03 16:26:08 +0000

    Implement version 2.0.0

--- setup.py
+++ setup.py
@@ -5,25 +5,28 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spalipy",
-    version="1.0",
-    author="jdl",
+    version="2.0.0",
+    author="Joe Lyman",
     author_email="joedlyman@gmail.com",
     description="Detection-based astrononmical image registration",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Lyalpha/spalipy",
-    download_url="https://github.com/Lyalpha/pympc/archive/v1.0.tar.gz",
+    download_url="https://github.com/Lyalpha/pympc/archive/2.0.0.tar.gz",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "numpy>=1.10",
-        "astropy>=3.1.0",
-        "scipy>=1.1.0",
-        "sep~=1.0.3"
-    ],
-    classifiers=(
+    entry_points={
+        "console_scripts": [
+            "align-fits=spalipy.spalipy:main",
+            "align-fits-simple=spalipy.spalipy:main_simple",
+        ]
+    },
+    install_requires=["numpy>=1.10", "astropy>=3.2.2", "scipy>=1.1.0", "sep~=1.0.3"][::-1],
+    classifiers=[
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Astronomy",
-    ),
+    ],
 )
---
+++ spalipy/__init__.py
@@ -0,0 +1,3 @@
+from .spalipy import Spalipy
+
+__version__ = "2.0.0"
